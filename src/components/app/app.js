07f8cc65061b7aec0b06e3bf541bf9f9a497250f
doppelgangger11 <gamenightmer@gmail.com>
2024-04-18 20:04:07 +0500

    Code refactoring v3

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,12 +1,9 @@
 import React, { Component } from "react";
-
 import Header from "../header";
 import RandomPlanet from "../random-planet";
 import ErrorButton from "../error-button";
 import ErrorIndicator from "../error-indicator";
-
 import "./app.css";
-
 import PeoplePage from "../people-page";
 import ItemList from "../item-list";
 import PersonDetails from "../person-details";
@@ -19,22 +16,17 @@ export default class App extends Component {
     showRandomPlanet: true,
     hasError: false,
   };
-
   toggleRandomPlanet = () => {
     this.setState((state) => {
       return { showRandomPlanet: !state.showRandomPlanet };
     });
   };
-
   componentDidCatch() {
     this.setState({ hasError: true });
   }
-
   render() {
     if (this.state.hasError) return <ErrorIndicator />;
-
     const planet = this.state.showRandomPlanet ? <RandomPlanet /> : null;
-
     return (
       <div className="container-fluid">
         <div className="row">
@@ -50,25 +42,31 @@ export default class App extends Component {
             <ErrorButton />
           </div>
         </div>
-        <PeoplePage />
 
+        <PeoplePage />
+        
         <div className="row">
           <div className="col-md-6">
             <ItemList
               onItemSelected={this.onPersonSelected}
               getData={this.swapiService.getAllPlanets}
+              renderItem={(item) => (
+                <span>
+                  {item.name} <button>!</button>
+                </span>
+              )}
             />
           </div>
           <div className="col-md-6">
             <PersonDetails personId={this.state.selectedPerson} />
           </div>
         </div>
-
         <div className="row">
           <div className="col-md-6">
             <ItemList
               onItemSelected={this.onPersonSelected}
               getData={this.swapiService.getAllStarships}
+              renderItem={(item) => item.name}
             />
           </div>
           <div className="col-md-6">
