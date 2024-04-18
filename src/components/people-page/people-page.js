27c3000c52f20da766825311cfa37a51561f279b
doppelgangger11 <gamenightmer@gmail.com>
2024-04-18 20:25:39 +0500

    I added HOC for Details lists

--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -1,40 +1,38 @@
 import React, { Component } from "react";
 import "./people-page.css";
 import ItemList from "../item-list";
-import PersonDetails from "../person-details";
-import ErrorIndicator from "../error-indicator";
+import ItemDetails from "../item-details";
 import SwapiService from "../../services/swapi-service";
 import Row from "../row";
-import ErrorButton from "../error-button";
 import ErrorBoundry from "../error-boundry";
 
 export default class PeoplePage extends Component {
   swapiService = new SwapiService();
+
   state = {
     selectedPerson: 3,
-    hasError: false,
   };
 
   onPersonSelected = (selectedPerson) => {
     this.setState({ selectedPerson });
   };
+
   render() {
-    if (this.state.hasError) return <ErrorIndicator />;
     const itemList = (
       <ItemList
         onItemSelected={this.onPersonSelected}
         getData={this.swapiService.getAllPeople}
       >
-        {(i) => `${i.name} (${i.birthYear})}`}
+        {(i) => `${i.name} (${i.birthYear})`}
       </ItemList>
     );
 
-    const personDetails = (
+    const itemDetails = (
       <ErrorBoundry>
-        <PersonDetails personId={this.state.selectedPerson} />
+        <ItemDetails itemId={this.state.selectedPerson} />
       </ErrorBoundry>
     );
 
-    return <Row left={itemList} right={personDetails} />;
+    return <Row left={itemList} right={itemDetails} />;
   }
 }
